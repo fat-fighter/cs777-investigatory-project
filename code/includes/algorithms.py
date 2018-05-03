a554f28d4aa83eaff2923083fd7c77e2f4ffbc69
Gurpreet Singh <g.singh6343@gmail.com>
2018-05-04 01:03:14 +0530

    Added code for mlp with pretrained weights

--- code/includes/algorithms.py
+++ code/includes/algorithms.py
@@ -17,7 +17,8 @@ from models.logistic import Logistic
 from sklearn.neural_network import MLPClassifier as MLP
 
 
-def softmax_regression(train_set, test_set, verbose, l_rate, n_classes, n_epochs, batch_size, tol):
+def softmax_regression(train_set, test_set, verbose, l_rate,
+                       n_classes, n_epochs, batch_size, tol):
     model = Softmax(
         data=train_set,
         n_classes=n_classes,
@@ -36,10 +37,10 @@ def softmax_regression(train_set, test_set, verbose, l_rate, n_classes, n_epochs
     return predictions, model
 
 
-def boosted_softmax_regression(
-        train_set, test_set, verbose, l_rate, n_classes,
-        n_epochs, batch_size, m_stop, eta, activation, plot_file
-):
+def boosted_softmax_regression(train_set, test_set, verbose,
+                               l_rate, n_classes, n_epochs,
+                               batch_size, m_stop, eta, activation,
+                               plot_file):
     model = Softmax(
         data=train_set,
         n_classes=n_classes,
@@ -83,19 +84,20 @@ def boosted_softmax_regression(
     np.save(plot_file, accuracies)
     plt.plot(accuracies)
     plt.savefig("plots/boosted-sft-plot.png")
-    plt.clf()
+    plt.close()
 
     return predictions, model
 
 
-def mlp(train_set, test_set, verbose, l_rate, n_classes, n_epochs, batch_size, hidden_size, tol):
+def mlp(train_set, test_set, verbose, l_rate, n_classes,
+        activation, n_epochs, batch_size, hidden_size, tol):
     model = MLP(
         hidden_layer_sizes=hidden_size,
         learning_rate_init=l_rate,
         batch_size=batch_size,
         max_iter=n_epochs,
         solver="sgd",
-        activation="tanh",
+        activation=activation,
         nesterovs_momentum=False,
         verbose=verbose,
         tol=tol,
@@ -106,7 +108,66 @@ def mlp(train_set, test_set, verbose, l_rate, n_classes, n_epochs, batch_size, h
 
     model.fit(train_X, train_Y)
 
-    predictions = model.predict(test_set[:, :-1])
+    predictions = model.predict_proba(test_set[:, :-1])
     predictions = np.argmax(predictions, axis=1)
 
     return predictions, model
+
+
+def pretrained_mlp(train_set, test_set, verbose, l_rate,
+                   n_classes, activation, n_epochs, batch_size,
+                   hidden_size, tol, bm):
+    model = MLP(
+        hidden_layer_sizes=hidden_size,
+        learning_rate_init=l_rate,
+        batch_size=batch_size,
+        max_iter=1,
+        solver="sgd",
+        activation=activation,
+        nesterovs_momentum=False,
+        verbose=verbose,
+        tol=tol,
+        warm_start=True
+    )
+
+    sample = np.asarray(train_set[0], dtype=int)
+    sample_x = sample[:-1]
+    sample_y = np.zeros(n_classes)
+    sample_y[sample[-1]] = 1
+    model.fit([sample_x], [sample_y], )
+
+    model.intercepts_[0][0] = bm.bias_input[0]
+    model.intercepts_[1][:] = bm.bias_hidden[:]
+
+    for hn in range(1, hidden_size):
+        model.intercepts_[0][hn] = bm.models[hn - 1][1].intercepts_[0][0]
+        model.intercepts_[1][:] += bm.models[hn - 1][0] * \
+            bm.models[hn - 1][1].intercepts_[1][:]
+
+    model.coefs_[0][:, 0] = bm.coefficients_input[:, 0]
+    model.coefs_[1][0, :] = bm.coefficients_hidden[:, 0]
+
+    for hn in range(1, hidden_size):
+        model.coefs_[0][:, hn] = bm.models[hn - 1][1].coefs_[0][:, 0]
+        model.coefs_[1][hn, :] = bm.models[hn - 1][0] * \
+            bm.models[hn - 1][1].coefs_[1]
+
+    if verbose == True:
+        predictions = model.predict_proba(test_set[:, :-1])
+        predictions = np.argmax(predictions, axis=1)
+
+        accuracy = np.mean(test_set[:, -1] == predictions)
+
+        print "Pretrained MLP, Activation = %s", activation
+        print "Initial Test Accuracy: %f" % accuracy
+
+    for _ in range(n_epochs):
+        model.fit(train_set[:, :-1], np.eye(n_classes)
+                  [np.asarray(train_set[:, -1], dtype=int)])
+
+    predictions = model.predict_proba(test_set[:, :-1])
+    predictions = np.argmax(predictions, axis=1)
+
+    accuracy = np.mean(predictions == test_set[:, -1])
+
+    return accuracy, model
--- code/includes/config.py
+++ code/includes/config.py
@@ -14,49 +14,26 @@ class Config:
         self.lr = lr
 
     def set_algorithm(self, algorithm):
-        # Logistic Regression
-        if algorithm == "log-reg":
-            self.n_epochs = 500
-            self.tol = 0.0001
-            self.lr = 0.1
-
-        # Gradient Boosted Logistic Regression
-        elif algorithm == "boosted-log-reg":
-            self.m_stop = 200
-            self.n_epochs = 200
-            self.lr = 0.1
-            self.eta = 0.5
-            self.eta_decay = 0.9
-            self.tol = 0.000001
-
-        # Single Class Multi Layered Perceptron
-        elif algorithm == "mlp":
-            self.n_epochs = 200
-            self.lr = 0.1
-
-            self.hidden_size = (50)
-
         # Softmax Regression
-        elif algorithm == "sft-reg":
+        if algorithm == "sft-reg":
             self.n_epochs = 200
             self.tol = 0.00001
             self.lr = 0.01
 
         # Boosted Softmax Regression
         elif algorithm == "boosted-sft-reg":
-            self.n_epochs = 50
+            self.n_epochs = 100
             self.tol = 0.00001
             self.lr = 0.01
 
-            self.m_stop = 100
+            self.m_stop = 50
             self.eta = 1.0
             self.eta_decay = 0.95
 
         # Multi Class Multi Layered Perceptron
         elif algorithm == "multi-mlp":
-            self.batch_size = 50
-            self.n_epochs = 200
-            self.lr = 0.01
+            self.n_epochs = 500
+            self.lr = 0.005
             self.tol = 0.00001
 
-            self.hidden_size = (60)
+            self.hidden_size = 50
